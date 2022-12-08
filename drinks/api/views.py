5ad60af87a6aa090713ac5d8ec530ee9922ec8a8
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-12-08 00:54:29 -0500

    serializer view called `drink_list`

--- drinks/api/views.py
+++ drinks/api/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from api.models import Drink
+from api.serializers import DrinkSerializer
 
 # Create your views here.
-def index(request):
-    return HttpResponse("<h1>Hello<h1>")
+
+def drink_list(request):
+    # get all the drinks
+    # serialize them
+    # return JSON
+
+
+    # Get all the drinks.
+    drinks = Drink.objects.all()
+
+    # Serialize them.
+    serializer = DrinkSerializer(drinks, many=True)
+
+    # Return JSON.
+    return JsonResponse({'drinks': serializer.data})
+
+
