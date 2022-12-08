b2307e0f9a2d805db298b2779d3784b65d61a395
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-12-07 23:39:08 -0500

    vaulted secrets

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e!a3&l!ne==f22$-^745031)gxnob*$^$q!m9^&45oa_@%n-hw'
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
