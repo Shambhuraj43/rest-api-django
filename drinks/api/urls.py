5c2c0b3c178a8413b0c90099258c0f028af3762b
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-12-08 01:45:49 -0500

    created API paths and endpoints

--- drinks/api/urls.py
+++ drinks/api/urls.py
@@ -4,5 +4,6 @@ from api import views
 app_name = 'api'
 
 urlpatterns = [
-    path('drinks/', views.drink_list, name='drinks'),
+    path('drinks/', views.drink_list, name='drink_list'),
+    path('drinks/<int:id>', views.drink_detail, name='drink_detail')
 ]
